ac08bda23e2d56dcafe11c8e2d431169740ba50d
libaichuan <libaichuan@nibirutech.com>
2019-08-14 19:40:20 +0800

    全排列

--- perm.go
+++ perm.go
@@ -8,6 +8,8 @@ import "fmt"
 //给定a,b,c  生成abc,acb,bac,bca,cab,cba
 
 //递归算法
+//固定任意一个元素，求剩下的全排列
+
 func PermRecursive(arr []string, beginIndex int) {
 	if beginIndex == len(arr) - 1 {
 		fmt.Println(arr)
@@ -16,8 +18,34 @@ func PermRecursive(arr []string, beginIndex int) {
 
 	for i := beginIndex; i < len(arr); i++ {
 		arr[beginIndex], arr[i] = arr[i], arr[beginIndex]
-		PermRecursive(arr, i)
+		PermRecursive(arr, beginIndex + 1)
 		arr[beginIndex], arr[i] = arr[i], arr[beginIndex]
+	}
+}
+
+func IsPermRecursiveRepeat(arr []string, beginIndex int, endIndex int) bool{
+	for temp := beginIndex; temp < endIndex; temp++ {
+		if arr[temp] == arr[endIndex] {
+			return true
+		}
+	}
+
+	return false
+}
+
+func PermRecursiveNoRepeat(arr []string, beginIndex int) {
+	if beginIndex == len(arr) - 1 {
+		fmt.Println(arr)
+		return
+	}
+
+	for i := beginIndex; i < len(arr); i++ {
+		isRepeat := IsPermRecursiveRepeat(arr, beginIndex, i)
+		if !isRepeat {
+			arr[beginIndex], arr[i] = arr[i], arr[beginIndex]
+			PermRecursiveNoRepeat(arr, beginIndex + 1)
+			arr[beginIndex], arr[i] = arr[i], arr[beginIndex]
+		}
 	}
 }
 
--- perm_test.go
+++ perm_test.go
@@ -1,1 +1,29 @@
 package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+
+func TestPermRecursive(t *testing.T) {
+
+	total := []string{"a", "b","c", "d", "e"}
+
+	for i := 1; i <= len(total); i++ {
+		PermRecursive(total[:i], 0)
+		fmt.Printf("\n\n")
+	}
+
+}
+
+func TestPermRecursiveNoRepeat(t *testing.T) {
+
+	total := []string{"a","a","b","b"}
+
+	for i := 1; i <= len(total); i++ {
+		PermRecursiveNoRepeat(total[:i], 0)
+		fmt.Printf("\n\n")
+	}
+
+}
